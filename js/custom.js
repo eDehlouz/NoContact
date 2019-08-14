9c56c383c5845ea51966bea280340c2f80b9b3c4
Erwan Dehlouz <dehlouz.erwan@gmail.com>
2019-08-14 14:55:15 +0200

    new v.

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,37 @@
+$('.btn-santos').click(function() {
+  $('.map-img').attr('src', 'img/Maps D.Santos-01.png');
+});
 
-
-
-$('.btn-santos').click(function(){
-    $('.map-img').attr('src', 'img/Maps D.Santos-01.png');
+$('.btn-santos1').click(function() {
+  $('.map-img').attr('src', 'img/Maps D.Santos-02.png');
+});
+$('.btn-santos2').click(function() {
+  $('.map-img').attr('src', 'img/Maps D.Santos-03.png');
+});
+$('.btn-santos3').click(function(){
+    $('.map-img').attr('src', 'img/Maps D.Santos-04.png');
     });
+    $('.btn-santos4').click(function(){
+        $('.map-img').attr('src', 'img/Maps D.Santos-05.png');
+        });
 
-$('.btn-nia').click(function(){
-    $('.map-img').attr('src', 'img/Maps NIA-06.png');
-    });
+$('.btn-nia').click(function() {
+  $('.map-img').attr('src', 'img/Maps NIA-06.png');
+});
+$('.btn-nia1').click(function() {
+  $('.map-img').attr('src', 'img/Maps NIA-07.png');
+});
+$('.btn-nia2').click(function() {
+  $('.map-img').attr('src', 'img/Maps NIA-08.png');
+});
 
 
 /* To be added for the result*/
-$('.btn-submit').click(function(){
+$('.btn-submit').click(function() {
   var resultDiv = $('.result').css('display');
-  if (resultDiv == "none"){
+  if (resultDiv == "none") {
     $('.result').toggle('slow');
+    $('#nov-gallery').toggle('fast');
     window.location.href = '#result';
     resultDiv = $('.result').css('display');
   }
